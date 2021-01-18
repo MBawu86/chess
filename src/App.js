ed156e28243ee95fc7f0eee4078f3c2e9e9cd218
Molly Behrens <algorithm.warrior.unlocked@gmail.com>
2021-01-18 15:58:55 -0600

    sets fen limits for piece mvmt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// useEffect allows defining of variables in function components (mutabel variables) 
+import React, {useState, useEffect, useRef} from 'react'
+import ChessBoard from 'chessboardjsx'
+import Chess, { chess } from 'chess.js'
 
+const container = {
+  marginTop: '2rem',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItem: 'center'
+}
 function App() {
+// set fen rules for chess pieces mvt
+  const [fen, setFen] = useState
+  ('start')
+
+  let game = useRef(null)
+
+    useEffect(() => {
+      game.current = new Chess()
+    },[])
+
+    const onDrop = ({sourceSquare, targetSquare}) => {
+      let move = game.current.move({
+        from: sourceSquare,
+        to: targetSquare
+      }) 
+        //checks for illegal moves
+        if(move === null) return 
+
+        // provides the fen string
+        setFen(game.current.fen())
+    }
+
+    const checkMate = ({sourceSquare, targetSquare})
+
+    const resetGame= () => {
+      game.current.clear();
+      game.current.reset();
+      setFen('start')
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+        // game over?
+        // is game.current null? no --> continue
+        // && game_over = true --> then
+        game.current && game.current.game_over() ? 
+        <div style = {{textAlign: 'center'}}>
+          <h1> GAME OVER </h1>
+          <button onclick = {resetGame}>New Game</button>         
+        </div>:
+        <span></span>
+      }
+    <div className="App" style = {container}>
+      
+{/* allows chess.js to place positionals in state */}
+      <ChessBoard position = {fen}
+      onDrop = {onDrop}/> 
+        </div>
+        </>
   );
 }
 
